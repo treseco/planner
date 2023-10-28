4745ecab1c4f09e20fae7ce926e0c217e0a04b73
ctrese0 <ctrese@umich.edu>
2023-10-28 16:52:43 -0400

    add minumum DEFAULT_DAY_WIDTH requirement

--- config.h
+++ config.h
@@ -2,6 +2,6 @@
 #define PLANNER_CONFIG_H
 
 #define DEFAULT_SAVE_PATH "/Users/ct/projects/plan/planner/tests/save.dat"
-#define DEFAULT_DAY_WIDTH 10
+#define DEFAULT_DAY_WIDTH 10 //minimum=6
 
 #endif
